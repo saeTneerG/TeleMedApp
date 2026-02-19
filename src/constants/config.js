98093ed5d5d687caeddc9bd7b8c0cca1345e8506
saeTneerG <sunlightmoondark3@gmail.com>
2026-02-19 22:58:46 +0700

    fix: include metered config for EAS build

---
+++ src/constants/config.js
@@ -0,0 +1,3 @@
+// src/constants/config.js
+export const METERED_DOMAIN = 'appcliniconline.metered.live'; // Replace with your Metered Domain
+export const METERED_SECRET_KEY = 'JDc2QQc9Lkr_cHkpDMQ-poCi6lABrzRM-IlwApTgmsP9p8dA'; // Replace with your Metered Secret Key
